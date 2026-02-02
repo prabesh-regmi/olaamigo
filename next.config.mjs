dbf6d51ed9e51086b4399644f7e04dfa6ced35fc
prabesh-regmi <me.prabesh.regmi@gmail.com>
2026-02-02 10:45:43 +0545

    fix css not working issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
+const repoName = "olaamigo";
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? `/${repoName}` : "";
+const assetPrefix = isProd ? `https://prabesh-regmi.github.io/${repoName}/` : "";
+
 const nextConfig = {
   output: "export",
+  basePath,
+  assetPrefix,
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-  basePath: "/olaamigo",
-  assetPrefix: "/olaamigo/",
 };
 
 export default nextConfig;
